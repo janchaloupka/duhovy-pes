79a70a99b5fb01a4a50cc6fbbc1af9bfec305257
Khubajsn <khubajsn@gmail.com>
2026-07-10 20:44:34 +0200

    Replace .ExecuteUpdate in UserIsMemberHandler with normal loop

--- src/Inkluzitron/Handlers/UserIsMemberHandler.cs
+++ src/Inkluzitron/Handlers/UserIsMemberHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Discord.WebSocket;
 using Inkluzitron.Data;
+using Inkluzitron.Utilities;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,13 +25,23 @@ public class UserIsMemberHandler : IHandler
     private async Task OnUserLeftAsync(SocketGuild guild, SocketUser user)
     {
         using var ctx = DatabaseFactory.Create();
-        await ctx.Users.Where(u => u.Id == user.Id).ExecuteUpdateAsync(u => u.SetProperty(dbUser => dbUser.IsMember, false));
+        await Patiently.HandleDbConcurrency(async () =>
+        {
+            var dbUser = await ctx.Users.FindAsync(user.Id);
+            dbUser.IsMember = false;
+            await ctx.SaveChangesAsync();
+        });
     }
 
     private async Task OnUserJoinedAsync(SocketGuildUser user)
     {
         using var ctx = DatabaseFactory.Create();
-        await ctx.Users.Where(u => u.Id == user.Id).ExecuteUpdateAsync(u => u.SetProperty(dbUser => dbUser.IsMember, true));
+        await Patiently.HandleDbConcurrency(async () =>
+        {
+            var dbUser = await ctx.Users.FindAsync(user.Id);
+            dbUser.IsMember = true;
+            await ctx.SaveChangesAsync();
+        });
     }
 
     private async Task OnGuildAvailableAsync(SocketGuild guild)
@@ -38,11 +49,34 @@ public class UserIsMemberHandler : IHandler
         _ = Task.Run(async () =>
         {
             await guild.DownloadUsersAsync().ConfigureAwait(false);
-            var currentMemberIds = guild.Users.Select(u => u.Id).ToList();
 
-            using var ctx = DatabaseFactory.Create();
-            await ctx.Users.Where(u => currentMemberIds.Contains(u.Id)).ExecuteUpdateAsync(u => u.SetProperty(dbUser => dbUser.IsMember, true));
-            await ctx.Users.Where(u => !currentMemberIds.Contains(u.Id)).ExecuteUpdateAsync(u => u.SetProperty(dbUser => dbUser.IsMember, false));
+            var currentMemberIds = guild.Users.Select(u => u.Id).ToHashSet();
+            var lastId = 0ul;
+
+            while (true)
+            {
+                using var ctx = DatabaseFactory.Create();
+
+                var noMoreUsers = await Patiently.HandleDbConcurrency(async () =>
+                {
+                    var batch = await ctx.Users.OrderBy(user => user.Id).Where(u => u.Id > lastId).Take(20).ToListAsync();
+                    foreach (var dbUser in batch)
+                    {
+                        dbUser.IsMember = currentMemberIds.Contains(dbUser.Id);
+                    }
+
+                    await ctx.SaveChangesAsync();
+
+                    if (batch.Count == 0)
+                        return true;
+
+                    lastId = batch.Last().Id;
+                    return false;
+                });
+
+                if (noMoreUsers)
+                    break;
+            }
         });
     }
 }
